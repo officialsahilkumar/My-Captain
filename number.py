72c59489bb31bc8ede722c4d45d4385a50fa1828
Sahil Kumar <94519002+officialsahilkumar@users.noreply.github.com>
2021-11-17 15:48:17 +0530

    Add files via upload

---
+++ number.py
@@ -0,0 +1,9 @@
+#Python program to print positive numbers in given range
+
+list1 = [25, 66, -41, 30, -155, -19, 15, 455, -2, 9]
+
+#iterating each number in the list
+
+for num in list1:
+    if num >=0:
+        print(num, end= " ")
